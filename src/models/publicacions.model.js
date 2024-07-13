0b68e52f182431e8167632636190ed33cbb6fee8
Molar <grubenmj@gmail.com>
2024-07-12 22:41:53 -0600

    compleated

--- src/models/publicacions.model.js
+++ src/models/publicacions.model.js
@@ -1,36 +1,36 @@
 const mongoose = require('mongoose')
 const modelName = 'Post'
 const schema = new mongoose.Schema({
-    title: {
-      type: String,
-      required: true,
-      minLength: 2,
-      maxLength: 100
+   title: {
+       type: String,
+       required: true,
+       minLength: 1,
+       maxLength: 100,
    },
    image: {
-      type: String,
-      minLength: 2,
-      required: false
+       type: String,
+       required: false,
+       minLength:10,
+       maxLength:1000,
    },
-    body: {
-      type: String,
-      required: true,
-      minLength: 2,
-      maxLength: 1000
-    },
-    user: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'user', 
-      // Asegúrate de que 'User' es el nombre correcto del modelo de usuarios
-    },
-    Updated_at: {
-        type: Date,
-        default: Date.now
-     },
-     created_at: {
-        type: Date,
-        default: Date.now
-     }
-})
+   body: {
+       type: String,
+       required: true,
+       minLength:1,
+       maxLength:1000,
+   },
+   user: {
+       type: mongoose.Schema.Types.ObjectId,
+       ref: "user",
+   },
+   created_at: {
+       type: Date,
+       default: Date.now,
+   },
+   update_at: {
+       type:Date,
+       default: Date.now,
+   },
+});
 
 module.exports = mongoose.model(modelName, schema)
